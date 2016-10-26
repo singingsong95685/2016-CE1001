d530274bdd3b1cb2eb061ad649da1190f2d65a63
sicachang <sicachang@gmail.com>
2016-10-26 21:37:49 +0800

    upload E1

---
+++ Exercise/Exercise#1/E1-10058XXXX.cpp
@@ -0,0 +1,20 @@
+/*****************************************
+* Exercise1
+* Name: 張閎翰
+* Student Number: 1XX5820XX
+*Course: 2016-CE1001-A or B
+******************************************/
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "系級:資工系1B"<<endl;
+    cout << "學號:10058XXXX" << endl;
+    cout << "姓名:XXX"<<endl;
+
+    return 0;
+}
