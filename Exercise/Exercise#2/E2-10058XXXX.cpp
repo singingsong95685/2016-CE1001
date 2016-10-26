ea5f574076dac17cd6ad7b3a08af59c371c5f400
sicachang <sicachang@gmail.com>
2016-10-26 21:36:00 +0800

    upload E2

---
+++ Exercise/Exercise#2/E2-10058XXXX.cpp
@@ -0,0 +1,33 @@
+/*****************************************
+* Exercise2
+* Name: TA
+* Student Number: 10058XXXX
+* Course: 2016-CE1003-A or B
+******************************************/
+#include <iostream>
+using namespace std;
+int main(){
+    int chineseScores = 0;
+    int mathScores = 0;
+    int englishScores = 0;
+    int scienceScores = 0;
+    int historyScores = 0;
+    float average = 0;
+    float sum=0;
+    cout<<"Chinese score: ";
+    cin>>chineseScores;
+    cout<<"Math score: ";
+    cin>>mathScores;
+    cout<<"English score: ";
+    cin>>englishScores;
+    cout<<"Science score: ";
+    cin>>scienceScores;
+    cout<<"History score: ";
+    cin>>historyScores;
+    //summing the scores.
+    sum = chineseScores+mathScores+englishScores+scienceScores+historyScores;
+    //sum / numbers of subject = average.
+    average = sum/5;
+    cout<<"Sum: "<<sum<<endl;
+    cout<<"Average: "<<average;
+}
