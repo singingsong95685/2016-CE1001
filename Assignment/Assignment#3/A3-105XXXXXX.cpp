1e3dcf9980e3922eac681aba8c54002f7aca13ce
singingsong95685 <singingsong95685@gmail.com>
2016-10-26 23:09:35 +0900

    Update A3-105XXXXXX.cpp

--- Assignment/Assignment#3/A3-105XXXXXX.cpp
+++ Assignment/Assignment#3/A3-105XXXXXX.cpp
@@ -5,32 +5,32 @@ using namespace std;
 int main()
 {
 	int n = 0;
-	int i = 0, j = 0;
+	int i = 0 , j = 0;
 	int count = 0;
 
-     cout << "input N:";
-     cin >> n;
-     cout <<"f("<< n << "):";
+	cout << "input N:";
+	cin  >> n;
+	cout << "f(" << n << "):";
 
-     for (i = 1; i <= n; i++)    //¦]factor(x) factor count (x)
+     	for (i = 1; i<=n ; i++)    //Â¦]factor(x) factor count (x)
 	{
 		if (n%i == 0)
-			{
-			cout << i<<" " ;
+		{
+			cout << i <<" ";
 			count++;
-			}
+		}
 	}
-    cout << endl;
+   	cout << endl;
 	cout << "factor count (" << n <<"):" << count << "\n";
 
 	if (n >= 2)                                //1~X prime number
 	{
-		cout <<"1~"<< n << "prime number:";   // 2 is only prime in even
+		cout << "1~" << n << "prime number:";   // 2 is only prime in even
 		if (n >= 2)
 			cout << "2" << " ";
 		for (i = 3; i < n; i++)
 		{
-			for (j = 2; j < i; j++)
+			for (j = 2; j<i ; j++)
 			{
 				if (i%j == 0)
 					break;
@@ -40,8 +40,8 @@ int main()
 		}
 		cout << endl;
 	}
+	
 	int k = 1;
-
 	cout << "1~"<< n <<"are multiples of 3 and 7:" ;
 
 	while (k <= n)
@@ -50,13 +50,12 @@ int main()
 		{
 			cout << k << " ";
 		}
-
 		k++;
-
 	}
+	
 	if(n<3)
         cout << "not exist" <<endl;
-
 	cout << endl;
+	
 	return 0;
 }
