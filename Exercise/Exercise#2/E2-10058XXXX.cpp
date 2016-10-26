d361ce1400bd0fe69af17dcab3f22cc200d7cc34
CK <sicachang@gmail.com>
2016-10-26 21:42:10 +0800

    調整空格

--- Exercise/Exercise#2/E2-10058XXXX.cpp
+++ Exercise/Exercise#2/E2-10058XXXX.cpp
@@ -14,20 +14,28 @@ int main(){
     int historyScores = 0;
     float average = 0;
     float sum=0;
+    
     cout<<"Chinese score: ";
     cin>>chineseScores;
+    
     cout<<"Math score: ";
     cin>>mathScores;
+    
     cout<<"English score: ";
     cin>>englishScores;
+    
     cout<<"Science score: ";
     cin>>scienceScores;
+    
     cout<<"History score: ";
     cin>>historyScores;
+    
     //summing the scores.
     sum = chineseScores+mathScores+englishScores+scienceScores+historyScores;
+    
     //sum / numbers of subject = average.
     average = sum/5;
+    
     cout<<"Sum: "<<sum<<endl;
     cout<<"Average: "<<average;
 }
